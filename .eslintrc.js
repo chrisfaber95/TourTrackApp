2be213d3cc846744da924e98361b76897bdadb4b
chrisfaber95 <chrisfaber95@gmail.com>
2019-05-19 11:53:57 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+	'globals': {
+		'gapi': false
+	},
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended'
+		// 'plugin:vue/recommended'
+  ],
+	// plugins: [
+	// 	'vue'
+	// ],
+  // add your custom rules here
+	rules: {
+		'nuxt/no-cjs-in-config': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/html-indent': ['error', 'tab'],
+		'vue/html-closing-bracket-newline': 'off',
+		'indent': [2, 'tab'],
+		'no-tabs': 'off'
+	}
+}
---
+++ nuxt.config.js
@@ -0,0 +1,99 @@
+const pkg = require('./package')
+
+module.exports = {
+	mode: 'universal',
+
+	/*
+  ** Headers of the page
+  */
+	head: {
+		htmlAttrs: {
+			lang: 'en'
+		},
+		title: pkg.name,
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+			{ hid: 'description', name: 'description', content: pkg.description }
+		],
+		script: [
+			{ src: 'https://apis.google.com/js/api.js' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Cousine:400,500,700' }
+		]
+	},
+
+	/*
+	** PWA manifest file
+	*/
+	manifest: {
+		name: pkg.name,
+		short_name: pkg.name,
+		lang: 'en-GB',
+		background_color: '#292929',
+		theme_color: '#292929',
+		description: pkg.description,
+		display: 'standalone'
+	},
+
+	/*
+  ** Customize the progress-bar color
+  */
+	loading: { color: '#fff' },
+
+	/*
+  ** Global CSS
+  */
+	css: [
+	],
+
+	/*
+  ** Plugins to load before mounting the App
+  */
+	plugins: [
+	],
+
+	/*
+  ** Nuxt.js modules
+  */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		// Doc: https://bootstrap-vue.js.org/docs/
+		'bootstrap-vue/nuxt',
+		'@nuxtjs/dotenv',
+		'@nuxtjs/pwa'
+	],
+	/*
+  ** Axios module configuration
+  */
+	axios: {
+		// See https://github.com/nuxt-community/axios-module#options
+	},
+
+	/*
+  ** Build configuration
+  */
+	build: {
+		/*
+    ** You can extend webpack config here
+    */
+		extend(config, ctx) {
+			// Run ESLint on save
+			if (ctx.isDev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	},
+	serverMiddleware: [
+		// API middleware
+		'~/server/api/index.js'
+	]
+}
